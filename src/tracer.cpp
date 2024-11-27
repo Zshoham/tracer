1d03e44641fe0330101cd2cd2dc9139b876a3a0d
Zshoham <ShohamZarfti@gmail.com>
2024-11-27 21:33:43 +0200

    feat: add basic support for multithreaded tracing

--- src/tracer.cpp
+++ src/tracer.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <fstream>
+#include <atomic>
+#include <shared_mutex>
 
 #include "tracer.hpp"
 
 static std::array<trace_point, TRACE_BUFFER_SIZE> trace_buffer;
-static size_t trace_buffer_index = 0;
+static std::atomic_size_t trace_buffer_index = 0;
+static std::shared_mutex flush_mutex;
 
 uint64_t time_ns() {
   struct timespec tv;
@@ -13,8 +16,15 @@ uint64_t time_ns() {
 }
 
 void trace_to_buffer(uint64_t location) {
-  trace_buffer[trace_buffer_index++ % TRACE_BUFFER_SIZE] = {location, time_ns()};
-  if (trace_buffer_index > trace_buffer.max_size()) {
+  // here we do a shared lock since 
+  // we allow concurrent trace calls (atomic index),
+  // but we dont allow tracing and flushing concurrently.
+  flush_mutex.lock_shared();
+  size_t index = trace_buffer_index.fetch_add(1, std::memory_order_relaxed);
+  trace_buffer[index % TRACE_BUFFER_SIZE] = {location, time_ns()};
+  flush_mutex.unlock_shared();
+  
+  if (index > trace_buffer.max_size()) {
     flush_trace_buffer(trace_buffer);
   }
 }
@@ -26,10 +36,15 @@ bool flush_trace_buffer(std::array<trace_point, TRACE_BUFFER_SIZE> &traces) {
         return false;  // Failed to open file
     }
 
+    // here we do an exclusive lock since when we flush nothing 
+    // else can touch the buffer.
+    flush_mutex.lock();
     while (trace_buffer_index > 0) {
-        trace_point t = trace_buffer[trace_buffer_index--];
+        size_t index = trace_buffer_index.fetch_sub(1, std::memory_order_relaxed);
+        trace_point t = trace_buffer[index];
         outFile.write(reinterpret_cast<const char*>(&t), sizeof(trace_point));
     }
+    flush_mutex.unlock();
 
     outFile.close();
     return true;
