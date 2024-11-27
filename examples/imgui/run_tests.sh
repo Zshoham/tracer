8d6a930bfbc8c261479fd89caf1166de627364c3
Zshoham <ShohamZarfti@gmail.com>
2024-11-27 21:34:45 +0200

    fix: use explicit shared library path when running imgui tests

--- examples/imgui/run_tests.sh
+++ examples/imgui/run_tests.sh
@@ -3,4 +3,4 @@
 cd ./imgui_test_engine/imgui_test_suite/
 
 make -j$(getconf _NPROCESSORS_ONLN)
-./imgui_test_suite -v1 -nopause -nogui -nocapture tests
+LD_LIBRARY_PATH=$CONDA_PREFIX/lib ./imgui_test_suite -v1 -nopause -nogui -nocapture tests
